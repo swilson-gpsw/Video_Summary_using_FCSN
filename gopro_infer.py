ddf688decd2316860c0554ff34b39329d707b557
swilson <swilson@gopro.com>
2020-11-12 20:11:37 -0800

    run goooglenet

--- gopro_infer.py
+++ gopro_infer.py
@@ -68,9 +68,9 @@ def infer_model(model, data, config):
     return score0, score1
 
 if __name__ == "__main__":
-    # data_fol = "/mnt/hd02/CVPR/csv_div320/"
-    # files = [data_fol + fi for fi in os.listdir(data_fol)]
-    files = ['/mnt/hd02/CVPR/csv_div320/594457e9546fd50001e4f637.csv']
+    data_fol = "/mnt/hd02/CVPR/csv_div320/"
+    files = [data_fol + fi for fi in os.listdir(data_fol)]
+    # files = ['/mnt/hd02/CVPR/csv_div320/594457e9546fd50001e4f637.csv']
 
     results_fol = '/mnt/hd02/CVPR/results_timed/'
     model = load_model()
